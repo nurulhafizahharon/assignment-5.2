cfe9418af9fd1d8cc4c9846aff07cc6a0a2037c1
nurulhafizahharon <nurulhafizahharon@outlook.com>
2023-10-31 01:35:53 +0800

    Upload for assignment

--- App.js
+++ App.js
@@ -1,10 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View, Alert } from "react-native";
+import { RadioButton } from "react-native-paper";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [coffee, setCoffee] = useState("I like coffee.");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Your Name: </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your name"
+          onChangeText={(newName) => setName(newName)}
+          defaultValue={name}
+        />
+      </View>
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Your Age: </Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Enter your age"
+          onChangeText={(newAge) => setAge(newAge)}
+          defaultValue={age}
+        />
+      </View>
+
+      <View style={styles.radioGroup}>
+        <Text style={styles.label}>Like Coffee? </Text>
+        <View style={styles.radioButton}>
+          <RadioButton
+            value="I like coffee."
+            status={coffee == "I like coffee." ? "checked" : "unchecked"}
+            onPress={() => setCoffee("I like coffee.")}
+          />
+          <Text style={styles.radioLabel}>Yes</Text>
+        </View>
+        <View style={styles.radioButton}>
+          <RadioButton
+            value="I do not like coffee."
+            status={coffee == "I do not like coffee." ? "checked" : "unchecked"}
+            onPress={() => setCoffee("I do not like coffee.")}
+          />
+          <Text style={styles.radioLabel}>No</Text>
+        </View>
+      </View>
+
+      <Button
+        title="Submit"
+        onPress={() =>
+          Alert.alert(
+            "Summary",
+            "My name is " + name + ". I am " + age + " years old. " + coffee
+          )
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +67,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: 20,
+    padding: 5,
+  },
+  label: {
+    flexDirection: "row",
+    flex: 1,
+    marginLeft: 10,
+  },
+  input: {
+    height: 30,
+    flexDirection: "row",
+    flex: 3,
+    margin: 5,
+    borderWidth: 1,
+    padding: 5,
+  },
+  inputGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  radioGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 10,
+  },
+  radioButton: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+  },
+  radioLabel: {
+    marginLeft: 8,
+    fontSize: 16,
+    color: "#333",
   },
 });
